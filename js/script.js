99bae4bb64a1e10160642b67bf1d5c53762a5902
epexa@bk.ru <epexa@bk.ru>
2021-10-24 16:25:14 +0300

    fix for old safari (iphone 5)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
-    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
-    typeSpeed: 100, // Скорость печати
-    startDelay: 500, // Задержка перед стартом анимации
-    backSpeed: 50, // Скорость удаления
-    loop: true // Указываем, повторять ли анимацию
+document.addEventListener('DOMContentLoaded', () => {
+
+    var typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+        stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+        typeSpeed: 100, // Скорость печати
+        startDelay: 500, // Задержка перед стартом анимации
+        backSpeed: 50, // Скорость удаления
+        loop: true // Указываем, повторять ли анимацию
+    });
+
 });
